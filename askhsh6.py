2266d98ec166bbab6ac4bed1c2a0ffcec1fe9591
aggy98 <36702789+aggy98@users.noreply.github.com>
2018-02-22 18:45:08 +0200

    Add files via upload

---
+++ askhsh6.py
@@ -0,0 +1,12 @@
+import calendar
+
+
+
+year = int(input("GIVE YEAR:"))
+
+month = int(input("GIVE MONTH:"))
+a = calendar.TextCalendar(calendar.SUNDAY)
+
+calendar = a.formatmonth(year, month)
+
+print (calendar)
---
+++ askhsh7.py
@@ -0,0 +1,30 @@
+import datetime
+
+wday = datetime.date.today().strftime("%A")
+
+day = datetime.date.today().day
+
+month = datetime.date.today().month
+
+year = datetime.date.today().year
+
+counter = 0
+
+for i in range(10):
+
+    while month <=12:
+
+        e = datetime.date(year,month,day)
+
+        if e.strftime("%A") == wday:
+
+           counter = counter + 1
+
+        month = month + 1
+
+    year = year + 1
+
+    month = 1
+
+print "There are ",counter,"days including today"
+
